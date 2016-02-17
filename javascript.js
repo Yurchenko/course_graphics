4d854ba9059364ea0bd8c98d3550851564b1749f
Yurchenko <yurchenkons@gmail.com>
2016-02-17 20:39:15 +0300

    lots of comments + TODO

--- javascript.js
+++ javascript.js
@@ -25,37 +25,47 @@ function init()
 //отрисовка на канвасе
 function draw()
 {
-  var w = 2; 
+  var w = 1; 
   var width = canvas.getAttribute("width");
   var height = canvas.getAttribute("height");
   var r = Math.min(width,height);
   
+  // всеобъемлющий граф будем называть "g0" и рисовать сами. Он будет  включать в себя все круги или надграфы
+  // соответственно обязательным пунктом в проверке JSON на валидность, будет проверка на то, что имя графа
+  // это единственный круг, который является сам себе родителем
+  current_graph = "g0"
   circle(width/2-w,height/2-w,r/2-2*w,w,"g0");
 
   //json
   //имя графа, описание вершин, размер окружности, в которую вписывается граф
   //с g начинаются графы (подграфы)
+  //чтобы не усугублять вложенностью, было решено подграфы писать на том же уровне, что и графы
+  // поэтому мы строим только деревья. А то где на остальное распологать то, а? 
+  // Ок, стороим только деревья, с остальным потом разберёмся\придумаем
   json = '[\
   {\
     "graph_name":"g0",\
     "verticies":[\
-      {"vertex_name":"1","radius":"4","adjacent_to":["2","3","4"]},\
-      {"vertex_name":"2","radius":"4","adjacent_to":["1"]},\
-      {"vertex_name":"3","radius":"4","adjacent_to":["1"]},\
-      {"vertex_name":"4","radius":"4","adjacent_to":["1"]}\
+      {"vertex_name":"1","radius":"1","adjacent_to":["2","3","4"]},\
+      {"vertex_name":"2","radius":"1","adjacent_to":["1"]},\
+      {"vertex_name":"3","radius":"1","adjacent_to":["1"]},\
+      {"vertex_name":"4","radius":"1","adjacent_to":["1"]}\
     ]\
   }\
   ]';
+
   plot(json);
 }
 
 //рисуем круг радиуса r, центром cx,cy. Толщина линии - w. Name - имя круга
-function circle (cx,cy,r,w,name) {
+function circle (cx,cy,r,w,name) 
+{
   var node = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
   node.setAttributeNS(null,"cx",cx); 
   node.setAttributeNS(null,"cy",cy); 
   node.setAttributeNS(null,"r",r); 
-  node.setAttributeNS(null,"name",name); 
+  node.setAttributeNS(null,"name",name);
+  node.setAttributeNS(null,"parent",current_graph); 
   node.style.stroke = "#000"; //Set stroke colour
   node.style.strokeWidth = w; //Set stroke width
   node.style.fill = "#FFF";
@@ -66,16 +76,28 @@ function circle (cx,cy,r,w,name) {
 //построение всего графа
 function plot(json)
 {
-  graphs = JSON.parse(json);
+  var graphs = JSON.parse(json);
+  // !TODO 
   graphs.forEach(check_json);  
+  //либо мы делаем проверку на то, что граф сначала указывается в списке смежностей, либо мы пишем
+  //цикл так, чтобы он крутился, пока всё не будет построено и пропускаем эти мелочи
   graphs.forEach(plot_graph); 
 } 
 
 //проверка подграфа на то, что все имена уникальны
 // 1) на уникальность имён вершин
+// на заметку = значения радиусов нам к хуям не упёрлись, поэтому при проверке можно
+// вообще тут два варианта 
+// а) можно "нормировать" это, то есть, чтобы хотя бы число были поменьше 
+// б) можно привести всё к интам, мб так лучше будет?)
+// итог: можно реализовать оба метода и протестировать какой из них лучше работает, если вообще будет виден 
+// прирост производительности
+// итог2: для этого нужно написать отдельную хуйню с тестами
+// нужен тест в глубину (супер дохуя вложений)
+// тест в ширину
 function check_json(graph)
 {
-  dict = []
+  var dict = []
   for (var i = 0; i < graph.verticies.length; i++) {
     if (dict[graph.verticies[i].name]==null)
       dict.push(graph.verticies[i].name)
@@ -104,7 +126,8 @@ function plot_graph(graph)
 function plot_vertex(vertex,graph)
 { 
   var verticies = vertex.adjacent_to;
-  for (var i = 0; i < verticies.length; i++) {
+  for (var i = 0; i < verticies.length; i++) 
+  {
     if (!(verticies[i] in graph.drawn))
     {  
       graph.drawn.push(verticies[i]);
